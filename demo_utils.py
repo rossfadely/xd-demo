32e43febd0f8a3c9dbe545268da4a8123d25ab8d
Ross Fadely <rossfadely@gmail.com>
2015-11-15 13:36:54 -0500

    all there

--- demo_utils.py
+++ demo_utils.py
@@ -60,3 +60,83 @@ def fake_data_plot(x_true, y_true, x, y, samples=None, mus=None, Vs=None):
                 ax[i].set_ylabel('$y$')
 
     pl.show()
+    
+def sdss_description_plot(x, fs=4, bins=64, tol=0.175):
+    """
+    Generate a plot to describe what the SDSS data looks like.
+    """
+    fig = pl.figure(figsize=(2 * fs, 2 * fs))
+    fig.subplots_adjust(left=0.1, right=0.95,
+                        bottom=0.1, top=0.95,
+                        wspace=0.25, hspace=0.25)
+       
+    pl.subplot(221)
+    pl.hist(x[:, 0], bins=bins, alpha=0.3, color='k', normed=True)
+    pl.xlim(16, 25)
+    pl.xlabel('r magnitude')
+    pl.ylabel('Relative Frequency')
+    
+    pl.subplot(222)
+    pl.plot(x[:, 0], x[:, -2], '.k', alpha=0.3, ms=1)
+    pl.plot([17, 23], [tol, tol], 'r', lw=2)
+    pl.xlim(17, 23)
+    pl.ylim(-0.1, 0.4)
+    pl.xlabel('r magnitude')
+    pl.ylabel('\'star\' - \'galaxy\' mag.')
+       
+    ax = pl.subplot(223)
+    ind = x[:, -1] < 0.25 * tol
+    pl.plot(x[ind, 2], x[ind, 3], '.k', alpha=0.3, ms=1)
+    ax.text(0.5, 0.95, '\'Stars\'', ha='center', va='top',
+           transform=ax.transAxes, fontsize=16)
+    pl.xlim(-1, 3)
+    pl.ylim(-1, 3)
+    pl.xlabel('g - r magnitude')
+    pl.ylabel('r - i magnitude')
+       
+    ax = pl.subplot(224)
+    ind = x[:, -1] > 2 * tol
+    pl.plot(x[ind, 2], x[ind, 3], '.k', alpha=0.3, ms=1)
+    ax.text(0.5, 0.95, '\'Galaxies\'', ha='center', va='top',
+           transform=ax.transAxes, fontsize=16)
+    pl.xlim(-1, 3)
+    pl.ylim(-1, 3)
+    pl.xlabel('g - r magnitude')
+    pl.ylabel('r - i magnitude')
+    pl.show()
+    
+def sdss_results_plot(x, samples, mus, Vs, fs=4, ind=[0, -2], tol=0.175):
+    """
+    Plot the results of the XD run.
+    """
+    fig = pl.figure(figsize=(3 * fs, fs))
+    fig.subplots_adjust(left=0.1, right=0.95,
+                        bottom=0.1, top=0.95,
+                        wspace=0.25, hspace=0.25)
+       
+
+    pl.subplot(131)
+    pl.plot(x[:, 0], x[:, -2], '.k', alpha=0.5, ms=1)
+    pl.plot([17, 23], [tol, tol], 'r', lw=2)
+    pl.xlim(17, 23)
+    pl.ylim(-0.1, 0.4)
+    pl.xlabel('r magnitude')
+    pl.ylabel('\'star\' - \'galaxy\' mag.')
+    
+    ax = pl.subplot(132)
+    for i in range(mus.shape[0]):
+        draw_ellipse(mus[i, ind], Vs[i, ind][:, ind], scales=[2], ax=ax,
+                     ec='k', fc='gray', alpha=0.2)
+    pl.xlim(17, 23)
+    pl.ylim(-0.1, 0.4)
+    pl.xlabel('r magnitude')
+
+    pl.subplot(133)
+    pl.plot(samples[:, 0], samples[:, -2], '.k', alpha=0.5, ms=1)
+    pl.plot([17, 23], [tol, tol], 'r', lw=2)
+    pl.xlim(17, 23)
+    pl.ylim(-0.1, 0.4)
+    pl.xlabel('r magnitude')
+    
+    pl.show()
+    
